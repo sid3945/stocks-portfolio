cced0abb6d0f7ce7d6183879100d31e7af0a88fa
Siddharth Upadhyay <siddharthupadhyay@MacBook-Pro-9.local>
2025-01-08 13:11:31 +0530

    env specific SSL config

--- config/db.js
+++ config/db.js
@@ -2,16 +2,28 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connectDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGODB_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true
-		});
-		console.log("MongoDB connected");
-	} catch (error) {
-		console.error("MongoDB connection error:", error);
-		process.exit(1);
-	}
+  try {
+    const isProduction = process.env.NODE_ENV === "production";
+    let options = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+    if (isProduction) {
+      const ca = fs.readFileSync("./global-bundle.pem");
+      options = {
+        ...options,
+        tls: true,
+        tlsCAFile: ca,
+        retryWrites: false,
+      };
+    }
+
+    await mongoose.connect(process.env.MONGODB_URI, options);
+    console.log("MongoDB / DocumentDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
